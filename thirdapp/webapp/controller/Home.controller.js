8809afcfb19a16ace5bdaacaa736b8d78c3e9028
Kayky Mattos <kaykymattos@outlook.com>
2024-04-01 15:28:24 -0300

    Putting messageBox information on the list

--- thirdapp/webapp/controller/Home.controller.js
+++ thirdapp/webapp/controller/Home.controller.js
@@ -1,15 +1,27 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/m/MessageBox"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast, MessageBox) {
+    "use strict";
 
-        return Controller.extend("com.lab2dev.thirdapp.controller.Home", {
-            onInit: function () {
+    return Controller.extend("com.lab2dev.thirdapp.controller.Home", {
+      onInit: function () {},
 
-            }
-        });
+      onPress: function (oEvent) {
+        //Origem do evento
+        const item = oEvent.getSource();
+
+        //Titulo do item
+        const itemTitle = item.getTitle();
+
+        //Mensagem a ser exibida
+        const message = `The item: "${itemTitle}" was clicked`;
+
+        //Exibe uma mensagem na tela
+        MessageBox.information(message);
+      },
     });
+  }
+);
